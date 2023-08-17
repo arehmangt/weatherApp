b4f57504c58d58207f116cf34db9a412d10ef9cf
Abdul Rehman <arehmangt@gmail.com>
2023-08-17 18:33:48 +0500

    minor change

--- src/redux/reducers/cityReducer.ts
+++ src/redux/reducers/cityReducer.ts
@@ -1,12 +1,18 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface CityState {
+  value: string[]; // Update this type according to your use case
+}
+
+const initialState: CityState = {
+  value: [], // Initial state for cities
+};
 
 export const citySlice = createSlice({
   name: "city",
-  initialState: {
-    value: [], // Initial state for cities
-  },
+  initialState,
   reducers: {
-    addCity: (state, action) => {
+    addCity: (state, action: PayloadAction<string>) => {
       state.value.push(action.payload); // Push the new city to the state array
     },
   },
